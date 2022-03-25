971564193e9d9a94479dffd8d9ff9f8cf8ee68ea
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-03-25 09:52:50 -0600

    Commit #3 /profile & /image

--- server.js
+++ server.js
@@ -58,13 +58,42 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length - 1])
 })
 
+// Get method for gettting /profile and ID --> /profile/2
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    } 
+  })
+  if (!found) {
+    res.status(404).json('User not found');
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    } 
+  })
+  if (!found) {
+    res.status(404).json('User not found');
+  }
+})
 
 app.listen(3000, () => {
   console.log('App is running on port 3000');
 });
 
 
-/*
+/* Planning
 / --> res = this is working
 /signin --> POST = success/fail
 /register --> POST = user
