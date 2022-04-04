da8ac6001a20c3e56ca7cde271f5da82fbf5584b
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-04-04 14:18:24 -0600

    Knex.js for connecting database to server

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    // port: 3306,
+    user: 'travisgeislinger',
+    password: '',
+    database: 'smartbrain'
+  }
+});
+
+
 
 // Creates app by running express
 const app = express();
@@ -12,91 +26,85 @@ app.use(cors());
 // for body-parser to work
 app.use(bodyParser.json());
 
-// Temporary Database
-const database = {
-  users: [
-    {
-      id: '1',
-      name: 'Doug',
-      email: 'doug@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '2',
-      name: 'Karen',
-      email: 'karen@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'doug@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send('Success');
 })
 
 // Signin
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-    res.json(database.users[0])
-  } else {
-    res.status(400).json('Error With Login');
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('Whatcha talkin\' about Willis'))
+      } else {
+        res.status(400).json('Wrong Credentials')
+      }
+    })
+    .catch(err => res.status(400).json('Wrong Credentials'))
 })
 
 // Register
 app.post('/register', (req, res) => {
-  const { email, name } = req.body;
-  database.users.push({
-    id: '3',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  // knex to insert users into the database -- create a transaction when you need to do more than 2 things at once
+  // Use trx instead of db 
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
   })
-  // Grabs last user in the array & Always need a response
-  res.json(database.users[database.users.length - 1])
+    .catch(err => res.status(400).json('Unable to register'));
 })
 
 // Get method for gettting /profile and ID --> /profile/2
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if (!found) {
-    res.status(404).json('User not found');
-  }
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('Not Found')
+      }
+    })
+    .catch(err => res.status(400).json('Error getting user'));
 })
 
+// Updates entries and increases the count
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  })
-  if (!found) {
-    res.status(404).json('User not found');
-  }
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('Not Able To Get Entries'))
 })
 
 // Always send sensitive info through https in a POST body 
