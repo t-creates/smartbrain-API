2d64fc7c428cd2b5c4df5f961c245b8ab26458da
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-04-04 16:50:19 -0600

    Cleaning up code

---
+++ controllers/register.js
@@ -0,0 +1,35 @@
+
+// Register
+const handleRegister = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  // knex to insert users into the database -- create a transaction when you need to do more than 2 things at once
+  // Use trx instead of db 
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  })
+    .catch(err => res.status(400).json('Unable to register'));
+}
+
+module.exports = {
+  handleRegister: handleRegister
+}
